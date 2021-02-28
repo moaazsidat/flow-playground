fae2e8f87b18ae3e3781571101a87315570cab3d
Moaaz Sidat <moaaz.omer@gmail.com>
2021-02-28 13:44:02 -0500

    auth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './config'; // Imports environment variables and configures FCL
 
 ReactDOM.render(
   <React.StrictMode>
